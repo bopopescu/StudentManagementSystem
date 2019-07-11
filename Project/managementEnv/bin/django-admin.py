1028fc8dd083c2438b0ce5e33d36e976ffc72a8d
IcourtDuraiSelvaraj <icourtduraiselvaraj@gmail.com>
2019-07-11 19:02:58 +0530

    project

---
+++ Project/managementEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/aspire1096/Project/managementEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Project/studentmanagementsystemproject/studentmanagementsystemapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentmanagementsystemappConfig(AppConfig):
+    name = 'studentmanagementsystemapp'
